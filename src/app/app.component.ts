b02f3728ccd59423a173fabc53d92d11a4fed502
shashank sharma <shashank.sharma@zincdigital.com>
2021-08-23 14:01:00 +0530

    add: barcode to qr code convertor done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { QRCodeComponent } from 'angularx-qrcode';
+import { BarcodeScannerLivestreamComponent } from "ngx-barcode-scanner";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'barcode-to-qrcode-convertor';
+  @ViewChild(BarcodeScannerLivestreamComponent, {static: false})
+  barcodeScanner: BarcodeScannerLivestreamComponent;
+
+  @ViewChild("qrcode", {static : true}) qrcode: QRCodeComponent
+  displayQr: boolean = false;
+  barcodeValue;
+  qrcodename: string;
+  title = 'generated-qrcode';
+  elementType: 'url' | 'canvas' | 'img' = 'url';
+  qrCodeValue: string;
+  qrDownloadLink: string;
+  
+  ngAfterViewInit() {
+    this.barcodeScanner.start();
+  }
+ 
+  onValueChanges(result) {
+    this.barcodeValue = result.codeResult.code;
+    if(this.barcodeValue) {
+      this.qrCodeValue = `MISHIPAY|EAN13|${this.barcodeValue}`;
+      this.displayQr = true;
+    }
+  }
+
+  downloadQrCode() {
+    this.qrDownloadLink = document.getElementsByTagName('img')[0].src;
+  }
 }
