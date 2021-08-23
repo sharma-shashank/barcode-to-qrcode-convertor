89547aff5ee894960f023ffb6fcb4137b0aa66c3
shashank sharma <shashank.sharma@zincdigital.com>
2021-08-23 14:00:39 +0530

    add: packages added for barcode and qr co

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BarcodeScannerLivestreamModule } from "ngx-barcode-scanner";
+import { QRCodeModule } from 'angularx-qrcode';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    QRCodeModule,
+    BarcodeScannerLivestreamModule
   ],
   providers: [],
   bootstrap: [AppComponent]
